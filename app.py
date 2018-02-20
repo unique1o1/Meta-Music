0a9aa234eccea38e73a5aa3abb018e33316de78c
yunik maharjan <yunik.maharjan@icloud.com>
2018-02-20 12:39:42 +0545

    threading replaced with multiprocessing

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-import threading
+import multiprocessing
 import time
 import random
 app = Flask(__name__)
@@ -8,19 +8,22 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-    t = threading.Thread(target=process)
-    t.start()
+    # t = multiprocessing.Process(target=process)
+    # t.start()
+
     return render_template('index.html')
 
 
+@app.route("/process", methods=["GET"])
 def process():
     a = int(random.random() * 10 % 10)
 
     print("inside this")
-    for i in range(10):
+    for i in range(5):
 
         time.sleep(2)
         print("sleep {}".format(a))
+    return render_template("process.html")
 
 
 if __name__ == "__main__":
