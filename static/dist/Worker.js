9158f1454f54c3fe4d9938d3ea54b7055c8966f1
yunik maharjan <yunik.maharjan@icloud.com>
2018-03-02 21:49:18 +0545

    comments removed

--- static/dist/Worker.js
+++ static/dist/Worker.js
@@ -16,16 +16,3 @@ this.onmessage = function(songs) {
   }
 };
 
-// xhr.onload = (e) => {
-//     if (xhr.readyState === 4) {
-//         if (xhr.status === 200) {
-//             console.log(this.postMessage(xhr.responseText));
-//         } else {
-//             console.error(xhr.statusText);
-//         }
-//     }
-// };
-// xhr.onerror = function (e) {
-//     console.error(xhr.statusText);
-// };
-// xhr.send(null);
