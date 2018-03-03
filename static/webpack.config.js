aef3eca09cc05e329de58597b690c5fc89467f18
yunik maharjan <yunik.maharjan@icloud.com>
2018-03-03 18:08:10 +0545

    local files used instead of CDN

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,7 +1,14 @@
 const webpack = require("webpack");
 
 const config = {
-  entry: __dirname + "/js/index.js",
+  entry: [
+    __dirname + "/js/index.js",
+    __dirname + "/js/app.js",
+    __dirname + "/js/card.js",
+    __dirname + "/js/bootstrap.min.js",
+
+    __dirname + "/js/jquery-3.3.1.min.js"
+  ],
 
   output: {
     path: __dirname + "/dist",
