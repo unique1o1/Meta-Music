1160fa5d8069445f570a4c0cf8d9dbae49bde5ed
yunik maharjan <yunik.maharjan@icloud.com>
2018-05-26 08:47:14 +0545

    argument parser fully added

--- audio_recognization_tester.py
+++ audio_recognization_tester.py
@@ -6,6 +6,9 @@ import warnings
 import argparse
 
 
+def initialize():
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Meta-Music: Audio Fingerprinting")
@@ -17,8 +20,38 @@ if __name__ == '__main__':
     parser.add_argument('-r', '--recognize', nargs=2,
                         help='Recognize what is songs\n'
                         'Usage: \n'
-                        '--recognize file path/to/file \n')
+                        '--recognize path/to/file \n')
     args = parser.parse_args()
     if not args.fingerprint and not args.recognize:
         parser.print_help()
         sys.exit(0)
+    if args.fingerprint:
+        # Fingerprint all files in a directory
+        if len(args.fingerprint) == 2:
+            directory = args.fingerprint[0]
+            extension = args.fingerprint[1]
+            print("Fingerprinting all .%s files in the %s directory"
+                  % (extension, directory))
+            meta.fingerprint_directory(directory, ['.'+extension])
+
+        elif len(args.fingerprint) == 1:
+            filepath = args.fingerprint[0]
+            if os.path.isdir(filepath):
+                print(
+                    "Please specify an extension if you'd like to fingerprint a directory!")
+                sys.exit(1)
+            meta.fingerprint_file(filepath)
+
+    elif args.recognize:
+        # Recognize audio source
+        song = None
+        source = args.recognize[0]
+
+        if os.path.isdir(source):
+            print(
+                "Please specify an file if you'd like to recognize the song")
+            sys.exit(1)
+        song = meta.recognize(FileRecognizer, opt_arg)
+        print(song)
+
+    sys.exit(0)
