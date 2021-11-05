32b58abae166f6cb0aad0791b81d5662ee235134
Sourcery AI <>
2021-11-05 14:21:29 +0000

    'Refactored by Sourcery'

--- Metamusic/decoder.py
+++ Metamusic/decoder.py
@@ -30,10 +30,7 @@ def read(filename: str, limit: Optional[int]=None) -> Tuple[list, int]:
 
     data = np.fromstring(audiofile._data, np.int16)
 
-    channels = []
-    for chn in range(audiofile.channels):
-        channels.append(data[chn::audiofile.channels])
-
+    channels = [data[chn::audiofile.channels] for chn in range(audiofile.channels)]
     fs = audiofile.frame_rate
 
     return channels, fs
